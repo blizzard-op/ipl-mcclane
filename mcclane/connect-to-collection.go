00d3b125595c4b729cf31f79a8b498d567f9bc00
Erik Wong <k4orta@gmail.com>
2013-02-21 10:50:02 -0800

    Moved Mongo details into config file

--- mcclane/connect-to-collection.go
+++ mcclane/connect-to-collection.go
@@ -4,19 +4,10 @@ import (
 	"labix.org/v2/mgo"
 )
 
-const (
-	//__________dev
-	// DB_HOST  = "localhost"
-	// MONGO_DB = "ipl-mcclane-dev"
-	//__________prod
-	DB_HOST  = "media-cms-prd-mongodb-01.las1.colo.ignops.com:27017"
-	MONGO_DB = "ipl-mcclane-prd"
-)
-
 func ConnectToCollection(col string) (*mgo.Collection, *mgo.Session) {
-	session, err := mgo.Dial(DB_HOST)
+	session, err := mgo.Dial(config.DB_HOST)
 	if err != nil {
 		panic(err)
 	}
-	return session.DB(MONGO_DB).C(col), session
+	return session.DB(config.MONGO_DB).C(col), session
 }
--- mcclane/models/config-type.go
+++ mcclane/models/config-type.go
@@ -3,4 +3,6 @@ package models
 type Config struct {
 	PassKey    string
 	PassSecret string
+	DB_HOST    string
+	MONGO_DB   string
 }
